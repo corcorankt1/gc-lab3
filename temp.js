c28be9381d6064e8a564fe2834f4f8d241c1eab3
Katie Corcoran <corcorankt1@gmail.com>
2016-03-29 20:33:24 -0400

    Initial commit

---
+++ temp.js
@@ -0,0 +1,8 @@
+var ftemp = 72;
+var ctemp = 22;
+
+var celsius = Math.round((ftemp-32)* 5/9);
+var farenheit = Math.round(ctemp*9/5+32);
+
+console.log("The temperature in celsius is " + celsius);
+console.log("The temperature in farenheit is " + farenheit);
